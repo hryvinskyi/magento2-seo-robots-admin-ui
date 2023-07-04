e61f7e40f003acccf4a0fd61ae9d1d09a2e08af9
Vladymyr Hrivinskyi <volodymyr@hryvinskyi.com>
2023-07-04 14:54:55 +0300

    Update ArraySerialized.php

--- Model/Config/Backend/ArraySerialized.php
+++ Model/Config/Backend/ArraySerialized.php
@@ -93,29 +93,58 @@ class ArraySerialized extends SerializedArraySerialized
     }
 
     /**
-     * @param array|string $value
+     * Normalizes the given value.
      *
-     * @return array|string
+     * @param mixed $value The value to be normalized.
+     * @return mixed The normalized value.
      */
     private function normalizeValue($value)
     {
-        $sortAlphabet = static function ($elem1, $elem2) {
-            return $elem1['pattern'] > $elem2['pattern'];
-        };
-
-        $sortCondition = static function ($elem1, $elem2) {
-            return strlen($elem2['pattern']) > strlen($elem1['pattern'])
-                && strrpos(
-                    str_replace(['/', '*'], [' ', ''], $elem2['pattern']),
-                    str_replace(['/', '*'], [' ', ''], $elem1['pattern'])
-                ) !== false;
-        };
-
         if (is_array($value)) {
-            uasort($value, $sortAlphabet);
-            uasort($value, $sortCondition);
+            uasort($value, [$this, 'sortAlphabet']);
+            uasort($value, [$this, 'sortCondition']);
         }
 
         return $value;
+    }
+
+    /**
+     * Sorts an array of elements alphabetically based on the 'pattern' key.
+     *
+     * @param array $elem1 The first element to compare.
+     * @param array $elem2 The second element to compare.
+     * @return int Returns a negative, zero, or positive integer depending on whether the first element is less than,
+     *             equal to, or greater than the second element.
+     */
+    private function sortAlphabet($elem1, $elem2)
+    {
+        return $elem1['pattern'] <=> $elem2['pattern'];
+    }
+
+    /**
+     * Sorts two elements based on their pattern length and pattern occurrence in reversed form.
+     *
+     * @param array $elem1 The first element to compare.
+     * @param array $elem2 The second element to compare.
+     * @return int Returns -1 if the pattern of $elem1 appears in reversed form in $elem2, returns the comparison
+     *             between the length of $elem2's pattern and $elem1's pattern if they have different lengths, or
+     *             returns the comparison between $elem1's pattern and $elem2's pattern if none of the conditions above apply.
+     */
+    private function sortCondition($elem1, $elem2)
+    {
+        $lengthComparison = strlen($elem2['pattern']) <=> strlen($elem1['pattern']);
+
+        if ($lengthComparison !== 0) {
+            return $lengthComparison;
+        }
+
+        $elem1Replaced = str_replace(['/', '*'], [' ', ''], $elem1['pattern']);
+        $elem2Replaced = str_replace(['/', '*'], [' ', ''], $elem2['pattern']);
+
+        if (strrpos($elem2Replaced, $elem1Replaced) !== false) {
+            return -1;
+        }
+
+        return $elem1Replaced <=> $elem2Replaced;
     }
 }
